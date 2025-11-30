b99ef8a6940aa16486628ed89598580725accd5a
Mustafa Fatih Can <canmustafafatih@gmail.com>
2025-11-30 14:11:58 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/mfc/logistics/cargo_management_api/CargoManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.mfc.logistics.cargo_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargoManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mfc/logistics/cargo_management_api/CargoManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfc.logistics.cargo_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
